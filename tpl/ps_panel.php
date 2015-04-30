89aac7b2ab4367f86293e083247b80eee155e7b9
modshirnk <hello@modshrink.com>
2015-04-30 19:17:56 +0900

    modify: gettext処理

--- tpl/ps_panel.php
+++ tpl/ps_panel.php
@@ -2,37 +2,37 @@
 	<div id="ps_head">
 		<ul>
 			<li><ul id="ps_all">
-				<li class="ps_filter"><a href="#">All Timeline</a></li>
+				<li class="ps_filter"><a href="#"><?php _e( 'All Timeline', 'prime-timeline' ); ?></a></li>
 			</ul><li>
 			<li><ul id="ps_file">
-				<li class="ps_filter"><a href="#">Loaded Files</a></li>
-				<li class="ps_show"><a href="#">+show</a></li>
-				<li class="ps_hide"><a href="#">-hide</a></li>
+				<li class="ps_filter"><a href="#"><?php _e( 'Loaded Files', 'prime-timeline' ); ?></a></li>
+				<li class="ps_show"><a href="#">+<?php _e( 'show', 'prime-timeline' ); ?></a></li>
+				<li class="ps_hide"><a href="#">-<?php _e( 'hide', 'prime-timeline' ); ?></a></li>
 			</ul><li>
 			<li><ul id="ps_sql">
-				<li class="ps_filter"><a href="#">SQL Queries</a></li>
-				<li class="ps_show"><a href="#">+show</a></li>
-				<li class="ps_hide"><a href="#">-hide</a></li>
+				<li class="ps_filter"><a href="#"><?php _e( 'SQL Queries', 'prime-timeline' ); ?></a></li>
+				<li class="ps_show"><a href="#">+<?php _e( 'show', 'prime-timeline' ); ?></a></li>
+				<li class="ps_hide"><a href="#">-<?php _e( 'hide', 'prime-timeline' ); ?></a></li>
 			</ul><li>
 			<li><ul id="ps_hook">
-				<li class="ps_filter"><a href="#">All Hooks</a></li>
-				<li class="ps_show"><a href="#">+show</a></li>
-				<li class="ps_hide"><a href="#">-hide</a></li>
+				<li class="ps_filter"><a href="#"><?php _e( 'All Hooks', 'prime-timeline' ); ?></a></li>
+				<li class="ps_show"><a href="#">+<?php _e( 'show', 'prime-timeline' ); ?></a></li>
+				<li class="ps_hide"><a href="#">-<?php _e( 'hide', 'prime-timeline' ); ?></a></li>
 			</ul><li>
 			<li><ul id="ps_hook_do">
-				<li class="ps_filter"><a href="#">Registerd Hooks</a></li>
-				<li class="ps_show"><a href="#">+show</a></li>
-				<li class="ps_hide"><a href="#">-hide</a></li>
+				<li class="ps_filter"><a href="#"><?php _e( 'Registerd Hooks', 'prime-timeline' ); ?></a></li>
+				<li class="ps_show"><a href="#">+<?php _e( 'show', 'prime-timeline' ); ?></a></li>
+				<li class="ps_hide"><a href="#">-<?php _e( 'hide', 'prime-timeline' ); ?></a></li>
 			</ul><li>
 			<li><ul id="ps_hook_pass">
-				<li class="ps_filter"><a href="#">Passed Hooks</a></li>
-				<li class="ps_show"><a href="#">+show</a></li>
-				<li class="ps_hide"><a href="#">-hide</a></li>
+				<li class="ps_filter"><a href="#"><?php _e( 'Passed Hooks', 'prime-timeline' ); ?></a></li>
+				<li class="ps_show"><a href="#">+<?php _e( 'show', 'prime-timeline' ); ?></a></li>
+				<li class="ps_hide"><a href="#">-<?php _e( 'hide', 'prime-timeline' ); ?></a></li>
 			</ul><li>
 			<li><ul id="ps_slow">
-				<li class="ps_filter"><a href="#">Slow Points</a></li>
-				<li class="ps_show"><a href="#">+show</a></li>
-				<li class="ps_hide"><a href="#">-hide</a></li>
+				<li class="ps_filter"><a href="#"><?php _e( 'Slow Points', 'prime-timeline' ); ?></a></li>
+				<li class="ps_show"><a href="#">+<?php _e( 'show', 'prime-timeline' ); ?></a></li>
+				<li class="ps_hide"><a href="#">-<?php _e( 'hide', 'prime-timeline' ); ?></a></li>
 			</ul><li>
 			<li><ul>
 				<li><input type='text' id='ps_slow_search' value='10' /></li>
--- tpl/ps_table_header.php
+++ tpl/ps_table_header.php
@@ -1,15 +1,14 @@
-<p><?php echo sprintf( 'Total execution time (%.2f msec)', $exec); ?></p>
+<p><?php echo sprintf( __( 'Total execution time (%.2f msec)', 'prime-timeline' ), $exec ); ?></p>
 <table>
 	<tbody>
 		<tr id='ps_tr_head'>
-			<th width='5%'>No.</th>
-			<th width='5%'>Total<br />(msec)</th>
-			<th width='10%'>Type</th>
-			<th width='5%'>File</th>
-			<th width='30%'>Hook/SQL</th>
-			<th width='30%'>Detail</th>
-			<th width='5%'>Count</th>
-			<th width='5%'>Diff<br />(msec)</th>
-			<th width='5%'>This<br />(msec)</th>
+			<th width='5%'><?php _e( 'No.', 'prime-timeline' ); ?></th>
+			<th width='5%'><?php _e( 'Total', 'prime-timeline' ); ?><br />(msec)</th>
+			<th width='10%'><?php _e( 'Type', 'prime-timeline' ); ?></th>
+			<th width='5%'><?php _e( 'File', 'prime-timeline' ); ?></th>
+			<th width='30%'><?php _e( 'Hook/SQL', 'prime-timeline' ); ?></th>
+			<th width='30%'><?php _e( 'Detail', 'prime-timeline' ); ?></th>
+			<th width='5%'><?php _e( 'Count', 'prime-timeline' ); ?></th>
+			<th width='5%'><?php _e( 'Diff', 'prime-timeline' ); ?><br />(msec)</th>
+			<th width='5%'><?php _e( 'This', 'prime-timeline' ); ?><br />(msec)</th>
 		</tr>
-
